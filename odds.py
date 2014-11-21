9acdd976d6b0497b6dc9240e3dd4df5c3f3d919e
Eric Leung <etleung21@gmail.com>
2014-11-20 16:41:52 -0800

    extracts pathways with >1.5 odds ratio

--- odds.py
+++ odds.py
@@ -30,6 +30,10 @@ for n in files: # loop through list of files that should be directory
 ###################
 
 # focus on KEGG Pathway Genes
+# SAMPLE FORMAT
+# ID	PATHWAY TITLE	MEMBERS
+# 04142	Lysosome	AP3S2	CTSE	GALNS   ...
+# 04916	Melanogenesis	WNT10A	ADCY7	CREB3L3 ...
 with open("KEGG_Pathway_Genes.txt", "r") as fh:
     fh.readline().rstrip() # remove header
     
@@ -40,6 +44,10 @@ with open("KEGG_Pathway_Genes.txt", "r") as fh:
         kegg.append(words) # add to master KEGG pathway list
 
 # focus on H5N1 VN1203 UNIVERSE Probes
+# SAMPLE FORMAT
+# A_23_P100011	AP3S2
+# A_23_P100022	SV2B
+# A_23_P100111	CHP
 with open("H5N1_VN1203_UNIVERSE_Probes.txt", "r") as fh:
     fh.readline().rstrip() # remove header
 
@@ -49,9 +57,13 @@ with open("H5N1_VN1203_UNIVERSE_Probes.txt", "r") as fh:
         words = line.rstrip().split("\t") # split line by tab
         universe[words[0]] = words[1] # save in dictionary
 
-print universe["A_23_P100011"]
-
+    universeProbes = universe.values() # get all probes in universe
+    
 # focus on H5N1 VN1203 DE Probes
+# SAMPLE FORMAT
+# A_23_P100539	ABCC6
+# A_23_P100642	PNMT
+# A_23_P100704	MAPK7
 with open("H5N1_VN1203_DE_Probes.txt", "r") as fh:
     fh.readline().rstrip() # remove header
 
@@ -61,4 +73,37 @@ with open("H5N1_VN1203_DE_Probes.txt", "r") as fh:
         words = line.rstrip().split("\t") # split line by tab
         de[words[0]] = words[1] # save in dictionary
 
-print de["A_23_P100539"]
+    deProbes = de.values() # get all DE probes
+
+#################
+### ODD RATIO ###
+#################
+
+# variables/data types
+sigPathways = [] # list of pathways with signif num DE genes
+
+# matrix for odds ratio
+#                | DE Genes | Non-DE 
+# target pathway |    a     |    b 
+# non-pathway    |    c     |    d
+
+# loop through pathways to get genes
+for pathway in kegg: # loop through all pathways
+    name = pathway[0:2] # get pathway ID and pathway name
+    genes = pathway[2:] # get probes in certain pathway
+    genes = set(genes) # make a set of genes
+
+    # make other data into sets
+    universeSet = set(universeProbes)
+    deSet = set(deProbes)
+
+    # set values based on matrix shown above
+    nonDE = universeSet.difference(deSet) # get non-DE genes
+    a = float(len(genes.intersection(deSet)))
+    b = float(len(genes.intersection(nonDE)))
+    c = float(len(deSet.difference(genes)))
+    d = float(len(nonDE.intersection(universeSet.difference(genes))))
+
+    # find pathways with odds ratio greater than 1.5
+    if (a*d)/(b*c) > 1.5:
+        sigPathways.append(name)
