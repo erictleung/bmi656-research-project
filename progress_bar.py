da10792e7ae70259526e418915a73b14d86beb53
Eric Leung <etleung21@gmail.com>
2014-12-02 23:41:59 -0800

    update progress bar

--- progress_bar.py
+++ progress_bar.py
@@ -1,15 +1,16 @@
 import sys
 import time
 
-toolbar_width = 40
+toolbar_width = 75
 
 sys.stdout.write("[%s]" % (" " * toolbar_width))
 sys.stdout.flush()
 sys.stdout.write("\b" * (toolbar_width+1))
 
-for i in xrange(toolbar_width):
-    time.sleep(0.1)
-    sys.stdout.write("-")
-    sys.stdout.flush()
-
+for i in xrange(100):
+#   time.sleep(0.1)
+    num = 100/10
+    if i % num == 0:
+        sys.stdout.write("-")
+        sys.stdout.flush()
 sys.stdout.write("\n")    
