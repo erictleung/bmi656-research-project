08e547fff969a9b67d3bedfcf86c737ebffed7a3
Eric Leung <etleung21@gmail.com>
2014-12-06 22:40:28 -0800

    fix unix/dox charage return

--- main.sh
+++ main.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
-
 # Name:       Eric Leung
 # Assignment: Research Project
 # Date:       December 8th, 2014
+# Bash Ver:   3.2.53
 # Script:     main.sh
 # Desription: This is the main command to 
-
 # check for files needed for scripts ahead
-if [ -f "odds.py" ];
-then
+if [ -f "Dodds.py" ]; then
     echo "Script for first part of project exists."
 else
     echo "Script for first part doesn't exist."
-
+fi
 # perform OR on pathways and choose pathway
-pathway=$(python odds.py)
-
 # examine cross-species conservation of pathway genes
-python crossSpecies.py $pathway
+# python crossSpecies.py
