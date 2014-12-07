900ef61605145fa38b65cfae9f6d87c41a7aab0b
Eric Leung <etleung21@gmail.com>
2014-12-06 22:47:03 -0800

    uncomment crossSpecies.py from shell script

--- main.sh
+++ main.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # Name:       Eric Leung
 # Assignment: Research Project
 # Date:       December 8th, 2014
@@ -6,11 +7,14 @@
 # Script:     main.sh
 # Desription: This is the main command to 
 # check for files needed for scripts ahead
-if [ -f "Dodds.py" ]; then
+
+# perform OR on pathways and choose pathway
+if [ -f "odds.py" ]; then
     echo "Script for first part of project exists."
+    python odds.py
 else
     echo "Script for first part doesn't exist."
 fi
-# perform OR on pathways and choose pathway
+
 # examine cross-species conservation of pathway genes
-# python crossSpecies.py
+python crossSpecies.py
